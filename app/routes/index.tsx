c31200744a79b3533780733957e259a97d5484db
majudhu <majudhu.sup@gmail.com>
2022-08-10 20:15:02 +0500

    add database crud

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,69 @@
+import { json, redirect } from '@remix-run/node';
+import { Form, useLoaderData, useSubmit } from '@remix-run/react';
+import { db } from '~/utils/db.server';
+
+import type { Joke } from "@prisma/client";
+import type { ActionArgs } from "@remix-run/node";
+
+export async function loader() {
+  return json({ jokes: await db.joke.findMany() });
+}
+
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+  const { id, ...data } = Object.fromEntries(formData) as unknown as Joke;
+  switch (request.method) {
+    case "DELETE":
+      await db.joke.delete({ where: { id } });
+      break;
+    case "PUT":
+      await db.joke.update({ where: { id }, data });
+      break;
+    case "POST":
+      await db.joke.create({ data });
+      break;
+  }
+  return redirect("/");
+}
+
 export default function Index() {
+  const { jokes } = useLoaderData<typeof loader>();
+  const submit = useSubmit();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
+    <Form method="post">
+      <h1>Wanna hear a joke?</h1>
+      {jokes.map(({ id, name, content, updatedAt, createdAt }) => (
+        <div key={id} className="p-2 border">
+          <p>id: {id}</p>
+          <p>name: {name}</p>
+          <p>content: {content}</p>
+          <p>updatedAt: {updatedAt}</p>
+          <p>createdAt: {createdAt}</p>
+          <button
+            type="button"
+            name="id"
+            value={id}
+            onClick={(e) => submit(e.currentTarget, { method: "put" })}
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
+            update
+          </button>
+          <button
+            type="button"
+            onClick={() => submit({ id }, { method: "delete" })}
           >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+            delete
+          </button>
+        </div>
+      ))}
+
+      <label>
+        name: <input className="border" name="name" />
+      </label>
+      <label>
+        content: <input className="border" name="content" />
+      </label>
+      <button type="submit">Make a joke!</button>
+    </Form>
   );
 }
